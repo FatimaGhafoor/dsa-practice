0c557f234fd5b6b5cd87a1c90dcf9e9e1ba56288
FatimaGhafoor <channu82411@gmail.com>
2026-07-21 22:22:21 +0500

    fix: remove irrelevent variable count

--- arrays/problem9.js
+++ arrays/problem9.js
@@ -12,7 +12,6 @@ Output: false
 */
 
 var containsDuplicate = function (nums) {
-  let count = 0;
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] === nums[j]) return true;
