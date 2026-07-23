0f374d8a1ca93f7db096ad19708119b97a669a95
FatimaGhafoor <channu82411@gmail.com>
2026-07-24 00:01:52 +0500

    solution: problem leetcode#169 Majority Element

--- arrays/problem12.js
+++ arrays/problem12.js
@@ -1,17 +1,15 @@
-/* Given an array nums of size n, return the majority element.
+/* Majority Element - Leetcode#169
 
-The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
-
- 
-
-Example 1:
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
 
 Input: nums = [3,2,3]
 Output: 3
-Example 2:
 
 Input: nums = [2,2,1,1,1,2,2]
-Output: 2 */
+Output: 2 
+*/
 
 var majorityElement = function(nums) {
     let candidate = null;
