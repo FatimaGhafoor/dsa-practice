0f8247f2458751039b48e76605c469be294feaa7
FatimaGhafoor <channu82411@gmail.com>
2026-07-25 23:34:27 +0500

    solution: problem leetcode#383 solved by HashMap pattern

--- arrays/problem13.js
+++ arrays/problem13.js
@@ -22,3 +22,18 @@ var canConstruct = function(ransomNote, magazine) {
     }
     return true;
 };
+
+// -------------------------HashMap pattern------------------------
+    
+var canConstruct = function(ransomNote, magazine) {
+    const count = {};
+
+    for (let char of magazine) {
+        count[char] = (count[char] || 0) + 1;
+    }
+    for (let char of ransomNote) {
+        if (!count[char]) return false;
+        count[char]--;
+    }
+    return true;
+};
