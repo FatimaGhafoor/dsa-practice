fa4d049eb0a87e58e11760c8994d999bf2b3cffb
FatimaGhafoor <channu82411@gmail.com>
2026-08-23 11:41:22 +0500

    feat: solve problem 28 inclusive range

--- conditionals/level-3-math-and-number-logic/28-range-checker.js
+++ conditionals/level-3-math-and-number-logic/28-range-checker.js
@@ -1,4 +1,4 @@
-// #28 - Check whether a number lies within **[100, 999]**.
+// #28 - Check whether a number lies within [100, 999].
 function checkRange(num) {
   return num >= 100 && num <= 999;
 }
