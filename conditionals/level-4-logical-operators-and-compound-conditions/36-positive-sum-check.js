b0173e46a4c2fe6dacf25814b40852011a2c6864
FatimaGhafoor <channu82411@gmail.com>
2026-08-24 13:24:14 +0500

    feat: solve problem 36 positive sum constraints check

---
+++ conditionals/level-4-logical-operators-and-compound-conditions/36-positive-sum-check.js
@@ -0,0 +1,5 @@
+// 36 - Check whether two numbers are both positive AND their sum is less than 100.
+
+function isPositiveAndLessThan100(num1, num2) {
+  return num1 > 0 && num2 > 0 && num1 + num2 < 100;
+}
