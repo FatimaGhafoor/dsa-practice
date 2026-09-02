a75235705249857b8c15fa33e7ae649335adcd98
FatimaGhafoor <channu82411@gmail.com>
2026-09-02 23:22:09 +0500

    feat: add printSquares function with complexities

---
+++ loops/level-3-mathematical-and-logical-problem/21-squares-1-to-n.js
@@ -0,0 +1,16 @@
+// Print the squares of numbers from 1 to n.
+
+function printSquares(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i * i);
+  }
+}
+
+printSquares(5);
+
+/* 
+Approach: Iterate from 1 to n using a loop and multiply each number by itself to find and print its square.
+
+Time Complexity: O(n);
+Space Complexity: O(1);
+*/
